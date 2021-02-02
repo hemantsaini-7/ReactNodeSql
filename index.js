e58da1a5bd812aa7c7e699c9fd360dbae1961e31
Hemant Saini <hsainioff@gmail.com>
2021-02-02 23:31:47 +0530

    Mongo Connection

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const home = require('./routes/home');
 const genres = require('./routes/genres');
 const customers = require('./routes/customers');
 
-mongoose.connect('')
-        .then(()=> console.log("Connecting to MongoDB"))
-        .catch((err)=> console.error("Failed to Connect",err.message));
+mongoose.connect('mongodb+srv://hemant:hemant@merncrud.mdkc5.mongodb.net/hemant?retryWrites=true&w=majority',{ useNewUrlParser: true, useUnifiedTopology: true  })
+        .then(()=> console.log("Connecting to MongoDB!"))
+        .catch((err)=> console.error("Failed to Connect", err.message));
 
 app.use('/',home);
 app.use('/api/genres',genres);
