e8b3623e2c4649035e42c66f4209d72179c6b5b2
Hemant Saini <hsainioff@gmail.com>
2021-03-04 19:49:17 +0530

    App JS

---
+++ REACTxExt/ReactWUI/src/App.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from "react";
+import Navbar from "./components/Navbar";
+import Landing from "./components/Landing";
+
+import "./App.css";
+
+function App() {
+  return (
+    <Fragment>
+      <Navbar />
+      <Landing />
+    </Fragment>
+  );
+}
+
+export default App;
