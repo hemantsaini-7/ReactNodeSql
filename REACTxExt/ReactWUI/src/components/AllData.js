4367f94f6c53b8d17a41a49e3886c4417a23fbe0
Hemant Saini <hsainioff@gmail.com>
2021-03-05 20:35:53 +0530

    All Data JS

---
+++ REACTxExt/ReactWUI/src/components/AllData.js
@@ -0,0 +1,54 @@
+export const AllData = [
+  {
+    id: 1,
+    head: "Card 1",
+    imgCard:
+      "https://image.shutterstock.com/image-photo/ancient-temple-ruins-gadi-sagar-260nw-786126286.jpg",
+    bottom:
+      "lorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlo",
+  },
+
+  {
+    id: 2,
+    head: "Card 2",
+    imgCard:
+      "https://images.unsplash.com/photo-1494548162494-384bba4ab999?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80",
+    bottom:
+      "lorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlo",
+  },
+
+  {
+    id: 3,
+    head: "Card 3",
+    imgCard: "",
+    bottom:
+      "lorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlo",
+  },
+
+  {
+    id: 4,
+    head: "Card 4",
+    imgCard:
+      "https://image.shutterstock.com/image-photo/bright-spring-view-cameo-island-260nw-1048185397.jpg",
+    bottom:
+      "lorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlo",
+  },
+
+  {
+    id: 5,
+    head: "Card 5",
+    imgCard:
+      "https://static.toiimg.com/thumb/72975551.cms?width=680&height=512&imgsize=881753",
+    bottom:
+      "lorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlo",
+  },
+
+  {
+    id: 6,
+    head: "Card 6",
+    imgCard:
+      "https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80",
+    bottom:
+      "lorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlolorwemlorwemlorwemlorwemlorwemlo",
+  },
+];
